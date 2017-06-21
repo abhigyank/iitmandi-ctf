bc6cf9cac14a2f232675139b03665384310cf3e0
Abhigyan Khaund <mail@abhigyan.xyz>
2017-06-21 13:24:59 +0530

    Fix bugs

--- public/js/script.js
+++ public/js/script.js
@@ -13,7 +13,9 @@ $(document).ready(function() {
   $('textarea').focus();
 });
 $('textarea').blur(function(){
-  $('textarea').focus();
+   setTimeout(function(){
+    $('textarea').focus();    
+  }, 50);
 });
 var temp_command = '';
 function reset(){
@@ -24,6 +26,11 @@ function reset(){
 };
 $('textarea').keyup(function(e) {
   var command = $('textarea').val();
+  if(command.search('<')!=-1 || command.search('>')!=-1){
+    alert('> or < not allowed.');
+    $('textarea').val($('textarea').val().substring(0,$('textarea').prop("selectionStart")-1));
+    return;
+  }
   command = command.replace(/(\r\n|\n|\r)/gm,"");
   if(e.which==38  && login!=1 && submit!=1){
     if(counter>=0){
