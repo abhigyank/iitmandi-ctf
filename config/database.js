059257600cca9e34bea960112378f5a67aaf659c
Abhigyan Khaund <20434085+abhigyank@users.noreply.github.com>
2019-01-30 14:49:44 +0530

    Move database detail to environment variables.

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-	'url' : "mongodb://localhost:27017/CMD"
+	'url' : `mongodb://${process.env.database_username}:${process.env.database_password}@${process.env.database_address}/${process.env.database_name}`
 }
